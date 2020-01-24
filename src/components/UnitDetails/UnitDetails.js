429d09c6dd72582e993308502908981f1c206233
Yunus Onder <yunus.onder@procat.com.tr>
2020-01-24 17:55:27 +0300

    router eklendi

---
+++ src/components/UnitDetails/UnitDetails.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function UnitDetails() {
+    return (
+      <div>
+       <h2>UnitDetails</h2>
+     </div>
+    );
+  }
+
+export default UnitDetails
--- src/index.js
+++ src/index.js
@@ -3,14 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { combineReducers, createStore } from 'redux';
-
-function reducer() {
-    return 'state'
-}
-
-const store = createStore(reducer)
-console.log(store.getState())
 ReactDOM.render( < App / > , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
